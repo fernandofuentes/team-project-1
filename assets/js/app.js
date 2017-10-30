b056da519c94bda056d46a50edc0bf8f3e042caa
Fernando Fuentes <fernando.fuentes1113@gmail.com>
2017-10-29 22:43:16 -0500

    added js and got results showing in console

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,28 @@
-/*writing this comment directly from github to see if i can do a pull request. */
+// displayMovieInfo function re-renders the HTML to display the appropriate content
+function displayTriviaQuestion() {
+
+  var question = $(this).attr("data-name");
+  var queryURL = "https://opentdb.com/api.php?amount=9";
+
+  // Creating an AJAX call for the specific movie button being clicked
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+
+    // Creating a div to hold the question
+    //  var questionDiv = $("<div class='question'>");
+
+    // Storing the rating data
+    var response = response.results;
+    console.log(response);
+
+    var question = response.results;
+    // console.log(question);
+
+  });
+
+
+}
+
+displayTriviaQuestion();
