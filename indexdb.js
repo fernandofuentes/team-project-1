11dc77865dea20a28dfd3cf3679e5ae42c23f46e
Fernando Fuentes <fernando.fuentes1113@gmail.com>
2017-11-04 10:40:24 -0500

    Delete indexdb.js
    Dan wanted this Deleted

--- indexdb.js
+++
@@ -1,9 +0,0 @@
-window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
- 
-window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
-window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
- 
-if (!window.indexedDB) {
-   window.alert("Your browser doesn't support a stable version of IndexedDB.")
-}
-Open an IndexedDB database
