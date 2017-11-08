996cd3ef22b0f34b6c2ce9ca90abbb43f8f11c36
Fernando Fuentes <fernando.fuentes1113@gmail.com>
2017-11-07 20:39:45 -0600

    trying to fix problems

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,106 +1,88 @@
 $(document).ready(function() {
+  function renderQuestion() {
+
+    var question = $(this).attr("data-name");
+    var queryURL = "https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple";
+
+    // Creating an AJAX call for the specific movie button being clicked
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+
+      // Creating a div to hold the question
+
+      // Storing the results data
+      var results = response.results;
+
+      // Storing the question data
+      var questionResponse = response.results["0"].question;
+
+      var correctAnswer = response.results["0"].correct_answer;
+
+      var incorrectAnswer1 = response.results["0"].incorrect_answers["0"];
+      var incorrectAnswer2 = response.results["0"].incorrect_answers["1"];
+      var incorrectAnswer3 = response.results["0"].incorrect_answers["2"];
 
 
-      function renderQuestion() {
-
-        var question = $(this).attr("data-name");
-        var queryURL = "https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple";
-
-        // Creating an AJAX call for the specific movie button being clicked
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).done(function(response) {
-
-          // Creating a div to hold the question
-          // var questionDiv = $("<div class='question'>");
-
-          // Storing the results data
-          var results = response.results;
-          console.log(results);
-
-          // Storing the question data
-          var question = response.results["0"].question;
-          // console.log(question);
-
-          var correctAnswer = response.results["0"].correct_answer;
-          // console.log(correctAnswer)
-
-          var incorrectAnswer1 = response.results["0"].incorrect_answers["0"];
-          var incorrectAnswer2 = response.results["0"].incorrect_answers["1"];
-          var incorrectAnswer3 = response.results["0"].incorrect_answers["2"];
-
-          // console.log(incorrectAnswer1)
-          // console.log(incorrectAnswer2)
-          // console.log(incorrectAnswer3)
-
-          // var choicesOrder = [correctAnswer, incorrectAnswer1, incorrectAnswer2, incorrectAnswer3]
-          // console.log(choicesOrder)
+      // Correct and Incorrect Choices array
+      var choices = [correctAnswer, incorrectAnswer1, incorrectAnswer2, incorrectAnswer3];
 
 
 
+      /////////////////////////////////////////////// RANDOM SHUFFLE
 
-          /////////////////////////////////////////////// RANDOM SHUFFLE
+      function shuffle(array) {
+        var currentIndex = array.length,
+          temporaryValue, randomIndex;
 
-          function shuffle(array) {
-            var currentIndex = array.length,
-              temporaryValue, randomIndex;
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
 
-            // While there remain elements to shuffle...
-            while (0 !== currentIndex) {
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
 
-              // Pick a remaining element...
-              randomIndex = Math.floor(Math.random() * currentIndex);
-              currentIndex -= 1;
+          // And swap it with the current element.
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+        }
 
-              // And swap it with the current element.
-              temporaryValue = array[currentIndex];
-              array[currentIndex] = array[randomIndex];
-              array[randomIndex] = temporaryValue;
-            }
+        return array;
+      }
 
-            return array;
-          }
+      arr = shuffle(choices);
 
-          // Used like so
-          var choices = [correctAnswer, incorrectAnswer1, incorrectAnswer2, incorrectAnswer3]
-          arr = shuffle(choices);
-          // console.log(choices);
 
-          /////////////////////////////////////////////// END RANDOM SHUFFLE
+
+      /////////////////////////////////////////////// END RANDOM SHUFFLE
 
 
 
 
-          // Display Question
+      // Display Question
+      $("button").click(function() {
+        $("table").hide();
+        var questionDiv = $("<div>");
+        questionDiv.addClass("question");
+        questionDiv.html(response.results["0"].question);
+        $(".question").html(question);
+      });
 
-          $("button").click(function() {
-            $("table").hide();
-            var questionDiv = $("<div>");
-            questionDiv.addClass("question");
-            questionDiv.html(response.results["0"].question);
-            $(".question").html(question);
-          });
+      // Display Answers
 
-          // Display Answers
+      // $("button").click(function() {
+      //
+      //   for (i = 0; i < choices.length; i++) {
+      //     list = "<li>" + choices[i] + "</li>";
+      //     $(".choices").append(list);
+      //     document.getElementById("choices").innerHTML = list;
+      //     list = "";
+      //   };
+      // });
 
-          $("button").click(function() {
-
-            for (i = 0; i < choices.length; i++) {
-              list = "<li>" + choices[i] + "</li>";
-              $(".choices").append(list);
-              document.getElementById("choices").innerHTML = list;
-              list = "";
-
-              // var answersDiv = $("<div>");
-              // answersDiv.addClass("answers");
-              // answersDiv.html(choices);
-              // $(".choices").html(choices);
-            };
-          });
-
-
-
-        });
-      };
-      renderQuestion()
+    });
+  }
+});
+renderQuestion();
