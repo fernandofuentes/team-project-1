d350148a52334bdefed082c056d687d79abdb1cd
Fernando Fuentes <fernando.fuentes1113@gmail.com>
2017-11-07 18:24:07 -0600

    added my js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,7 @@
-// displayMovieInfo function re-renders the HTML to display the appropriate content
 function renderQuestion() {
 
   var question = $(this).attr("data-name");
-  var queryURL = "https://opentdb.com/api.php?amount=9";
+  var queryURL = "https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple";
 
   // Creating an AJAX call for the specific movie button being clicked
   $.ajax({
@@ -15,32 +14,109 @@ function renderQuestion() {
 
     // Storing the results data
     var results = response.results;
-    //console.log(results);
+    console.log(results);
 
     // Storing the question data
     var question = response.results["0"].question;
     // console.log(question);
 
-    // The ForLoop to display the number of questions
-    for (i = 0; i < results.length; i++) {
-      // console.log(response.results[i].question);
-      // Next for lines creates the Questions Divs
+    var correctAnswer = response.results["0"].correct_answer;
+    // console.log(correctAnswer)
+
+    var incorrectAnswer1 = response.results["0"].incorrect_answers["0"];
+    var incorrectAnswer2 = response.results["0"].incorrect_answers["1"];
+    var incorrectAnswer3 = response.results["0"].incorrect_answers["2"];
+
+    // console.log(incorrectAnswer1)
+    // console.log(incorrectAnswer2)
+    // console.log(incorrectAnswer3)
+
+    // var choicesOrder = [correctAnswer, incorrectAnswer1, incorrectAnswer2, incorrectAnswer3]
+    // console.log(choicesOrder)
+
+
+
+
+    /////////////////////////////////////////////// RANDOM SHUFFLE
+
+    function shuffle(array) {
+      var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+    }
+
+    // Used like so
+    var choices = [correctAnswer, incorrectAnswer1, incorrectAnswer2, incorrectAnswer3]
+    arr = shuffle(choices);
+    console.log(choices);
+
+    /////////////////////////////////////////////// END RANDOM SHUFFLE
+
+
+
+
+    // Display Question
+
+    $("button").click(function() {
       var questionDiv = $("<div>");
       questionDiv.addClass("question");
-      questionDiv.html(response.results[i].question);
+      questionDiv.html(response.results["0"].question);
+      $(".question").html(question);
+    });
 
+    // Display Answers
+
+    $("button").click(function() {
       var answersDiv = $("<div>");
       answersDiv.addClass("answers");
       answersDiv.html(response.results[i].correct_answer);
+      $(".choices").html(choices);
+    });
+
+
+
+
+
+
+
+    // The ForLoop to display incorrectAnswers
+    for (i = 0; i < results.length; i++) {
+
+
+
+
+
+
+
+
+
+      // console.log(response.results[i].question);
+      // Next for lines creates the Questions Divs
+
+
 
       // console.log(response.results[i].question)
       // console.log(response.results[i].correct_answer)
 
-      $("#questions-dump").html(questionDiv)
-      $("#questions-dump").html(answersDiv)
+
+
+      // $("#questions-dump").append();
 
     }
   });
 };
-
 renderQuestion()
