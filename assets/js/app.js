88194e5f2f842a31b6ade856b305bb962a231e89
Fernando Fuentes <fernando.fuentes1113@gmail.com>
2017-11-09 18:17:33 -0600

    fixed appJS

--- assets/js/app.js
+++ assets/js/app.js
@@ -100,40 +100,6 @@ function renderQuestion() {
       document.getElementById('choices-div').appendChild(makeUL(options[0]));
 
     });
-
-
-
-    // $("button").click(function() {
-    //   // function makeUL(array) {
-    //   //   // Create the list element:
-    //   //   var list = document.createElement('ul');
-    //   //
-    //   //   for (var i = 0; i < array.length; i++) {
-    //   //     // Create the list item:
-    //   //     var item = document.createElement('li');
-    //   //
-    //   //     // Set its contents:
-    //   //     item.appendChild(document.createTextNode(array[i]));
-    //   //
-    //   //     // Add it to the list:
-    //   //     list.appendChild(item);
-    //   //   }
-    //   //
-    //   //   // Finally, return the constructed list:
-    //   //   return list;
-    //   // }
-    //   // // Display the choices
-    //   // document.getElementById('choices-div').appendChild(makeUL(options[0]));
-    // });
-
-
-
-
-
-
-
-
-
   });
 };
 
