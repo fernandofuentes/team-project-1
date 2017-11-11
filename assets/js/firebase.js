26ba89239c071d54231bbbbae0a5999fd73288ef
brandongatlin <brandongatlin1981@me.com>
2017-11-11 10:57:14 -0600

    commented out chat js code

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -94,55 +94,54 @@ function foo(isCorrect) {
 
 
 //begin chat js
-var chatData = database.ref("/chat");
-var username = displayName;
+// var chatData = database.ref("/chat");
 
 
 // name-submit on click.
-$('#name-submit').on("click", function(event) {
-  event.preventDefault();
-
-  username = $("#name-input").val().trim();
-
-  console.log("username:", username);
-
-  $('#name-form').hide(); // hide name form
-  $('#message-form').css("display", "initial"); // shows message form
-
-});
+// $('#name-submit').on("click", function(event) {
+//   event.preventDefault();
+//
+//   username = $("#name-input").val().trim();
+//
+//   console.log("username:", username);
+//
+//   $('#name-form').hide(); // hide name form
+//   $('#message-form').css("display", "initial"); // shows message form
+//
+// });
 
 
 //message-sumbit on click
-$('#message-submit').on("click", function(event) {
-
-  event.preventDefault();
-  var message = $("#message-input").val().trim();
-
-  $("#message-input").val(''); // clears out message input box
-
-  // this creates an object with our data to push up to firebase
-  var messageObj = {
-    name: username,
-    message: message,
-    time: firebase.database.ServerValue.TIMESTAMP
-  };
-
-  console.log(messageObj);
-
-  chatData.push(messageObj);
-
-});
+// $('#message-submit').on("click", function(event) {
+//
+//   event.preventDefault();
+//   var message = $("#message-input").val().trim();
+//
+//   $("#message-input").val(''); // clears out message input box
+//
+//   // this creates an object with our data to push up to firebase
+//   var messageObj = {
+//     name: username,
+//     message: message,
+//     time: firebase.database.ServerValue.TIMESTAMP
+//   };
+//
+//   console.log(messageObj);
+//
+//   chatData.push(messageObj);
+//
+// });
 
 
 // Update chat on screen when new message detected - ordered by 'time' value
-chatData.orderByChild("time").on("child_added", function(snapshot) {
-
-  $("#chat-box").append("<p>" + snapshot.val().name + "> " + snapshot.val().message + "</p>");
-
-  // Keeps div scrolled to bottom on each update.
-  $("#chat-box").scrollTop($("#chat-box")[0].scrollHeight);
-
-});
+// chatData.orderByChild("time").on("child_added", function(snapshot) {
+//
+//   $("#chat-box").append("<p>" + snapshot.val().name + "> " + snapshot.val().message + "</p>");
+//
+//   // Keeps div scrolled to bottom on each update.
+//   $("#chat-box").scrollTop($("#chat-box")[0].scrollHeight);
+//
+// });
 
 //auth js File
 // var database = firebase.database();
@@ -201,6 +200,7 @@ firebase.auth().signOut().then(function() {
   // An error happened.
 });
 
+//log on and log off button on click events
 $("#logOffFacebook").hide();
 
 $("#facebookBtn").on("click", function() {
